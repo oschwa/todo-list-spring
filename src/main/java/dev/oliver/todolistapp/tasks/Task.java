41371cd44a6dc9d67e2cb19eab4ba4581439e7a6
oschwa <oschwab33@gmail.com>
2024-11-14 10:36:43 -0800

    ass foundational Task structure

---
+++ src/main/java/dev/oliver/todolistapp/tasks/Task.java
@@ -0,0 +1,2 @@
+package dev.oliver.todolistapp.tasks;public class Task {
+}
---
+++ src/test/java/dev/oliver/todolistapp/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oliver.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
